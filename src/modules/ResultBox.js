54d30a47c5bc4388763a0c543cd2bea5e0525c71
chunwing-lam <chunwing.lam10@gmail.com>
2018-06-14 14:15:41 -0400

    Change to stateless functional component

--- src/modules/ResultBox.js
+++ src/modules/ResultBox.js
@@ -1,25 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import * as PortfolioService from '../PortfolioService';
 
-class ResultBox extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="result-box">
-        <div className="result-header-rows">
-          <div className="result-header-row">Dividend Average</div>
-          <div className="result-header-row">Dividend Total</div>
-        </div>
-        <div className="result-rows">
-          <div className="result-row">{PortfolioService.getDividendAverage(this.props.stocks)}</div>
-          <div className="result-row">{PortfolioService.getDividendTotal(this.props.stocks)}</div>
-        </div>
+const ResultBox = (props) => {
+  return (
+    <div className="result-box">
+      <div className="result-header-rows">
+        <div className="result-header-row">Dividend Average</div>
+        <div className="result-header-row">Dividend Total</div>
       </div>
-    );
-  }
+      <div className="result-rows">
+        <div className="result-row">{PortfolioService.getDividendAverage(props.stocks)}</div>
+        <div className="result-row">{PortfolioService.getDividendTotal(props.stocks)}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ResultBox;
